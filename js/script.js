6359f2c80d4f27ef12e14e5d1a50e9eb3a3e2654
ayidtheseptima <93842487+ayidtheseptima@users.noreply.github.com>
2025-08-02 14:31:12 +0300

    small screens + buger menu

--- js/script.js
+++ js/script.js
@@ -1,21 +1,40 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  navigation: {
-    nextEl: '.swiper-button-prev',
-    prevEl: '.swiper-button-next',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  direction: 'horizontal',
-  slidesPerView: 3,
-  loop: true,
-  loopedSlides: 3,
-  grabCursor: true,
-  freeMode: true,
-  spaceBetween: 16,
+const swiper = new Swiper(".swiper", {
+	navigation: {
+		nextEl: ".swiper-button-prev",
+		prevEl: ".swiper-button-next",
+	},
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	direction: "horizontal",
+	loop: true,
+	loopedSlides: 3,
+	grabCursor: true,
+	freeMode: true,
+	spaceBetween: 16,
+
+	breakpoints: {
+		1: {
+			slidesPerView: 1,
+			spaceBetween: 10,
+		},
+
+		1200: {
+			slidesPerView: 3,
+			spaceBetween: 16,
+		},
+	},
 });
 
+const burger = document.getElementById("header__burger");
+const menu = document.querySelector(".header__menu");
+const header = document.querySelector(".header");
+const contactInfo = document.querySelector(".header__contact-info");
 
-
+burger.addEventListener("click", () => {
+	burger.classList.toggle("active");
+	menu.classList.toggle("active");
+	contactInfo.classList.toggle("active");
+	header.classList.toggle("active");
+});
